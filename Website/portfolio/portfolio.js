2ba69dc4d963b6c7a7625efc473405bd9c2f8704
jtscreen <41080299+jtscreen@users.noreply.github.com>
2024-05-06 13:00:14 -0400

    Add files via upload

---
+++ Website/portfolio/portfolio.js
@@ -0,0 +1,14 @@
+function acting(){
+    location.replace("../pages/acting.html");
+}
+function music(){
+    alert("This page is currently undergoing maintenance. Please check back later or email with any questions. Thank you for your patience.")
+    //location.replace("../pages/music.html");
+}
+function photo(){
+    location.replace("../pages/photo.html");
+}
+function other(){
+    alert("This page is currently undergoing maintenance. Please check back later or email with any questions. Thank you for your patience.")
+    //location.replace("../pages/other.html");
+}
